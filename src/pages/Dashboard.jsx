bab89b759e064671f2c79e76bed08e32d487cb27
probeshnath <probeshn@gmail.com>
2023-12-21 22:53:27 +0600

    add task page design

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,8 +6,8 @@ const Dashboard = () => {
     <div className='bg-gray-300'>
       <div className="max-w-7xl mx-auto md:grid md:grid-cols-12  ">
         {/* side bar */}
-        <div className='bg-black text-white md:col-span-2 md:h-screen col-span-3'>
-          <ul>
+        <div className='bg-black text-white md:col-span-2 md:h-screen col-span-3 pl-3'>
+          <ul className=' flex flex-col gap-2'>
           <li><NavLink to="/dashboard" className={({ isActive }) => isActive ? "border-b-2 border-red-600 text-white font-semibold" : ""}> Dashboard </NavLink></li>
           <li><NavLink to="/dashboard/profile" className={({ isActive }) => isActive ? "border-b-2 border-red-600 text-white font-semibold" : ""}>   My Profile </NavLink></li>
           <li><NavLink to="/" className={({ isActive }) => isActive ? "border-b-2 border-red-600 text-white font-semibold" : ""}>  Home </NavLink></li>
--- src/pages/Dashboard/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../provider/AuthProvider'
 
 const DashboardHome = () => {
+    const {user} = useContext(AuthContext)
   return (
-    <div>DashboardHome</div>
+    <div>
+        <div className='flex gap-2 items-center'>
+            <img className='w-10 h-10 rounded-full' src={user?.photoURL} alt={user?.displayName} />
+            <div className="">
+                <h2 className='text-lg font-bold capitalize'>{user?.displayName}</h2>
+                <p className='text-sm text-gray-500 -mt-1'>{user?.email}</p>
+            </div>
+        </div>
+        <form className='flex py-3 flex-col gap-2'>
+            <input className='py-1 px-3 rounded-md outline-none' type="text" placeholder='Task Taile' name='title' />
+            <textarea className='py-1 px-3 rounded-md outline-none' name="description" placeholder='Task Description..'  cols="30" rows="10"></textarea>
+            <input className='py-1 px-3 rounded-md outline-none' type="date" name="deadline" placeholder='Deadline'  />
+            <select className='py-1 px-3 rounded-md outline-none' name="priority" id="">
+                <option value="low">Select your priority</option>
+                <option value="low">Low</option>
+                <option value="moderate">Moderate</option>
+                <option value="high">High</option>
+            </select>
+            <button className='bg-blue-700 py-1 rounded-md hover:bg-black text-white'>Create Task</button>
+        </form>
+    </div>
   )
 }
 
